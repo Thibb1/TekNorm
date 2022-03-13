f8578fbc4cc08ef793e23141b98c8ed69af54349
Thibb1 <thibault2.brumaire@epitech.eu>
2022-03-13 23:10:27 +0100

    + testing install script

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@
 ## Makefile
 ##
 
-NAME	=	./TekNorm.pl
+DIR		=	$(shell pwd)
+NAME	=	$(DIR)/TekNorm.pl
 DEST 	=	/usr/local/bin
 OUT 	=	teknorm
 
@@ -13,13 +14,13 @@ all:
 	@echo "Use 'make install' to install TekNorm or 'make uninstall' to remove it"
 
 install:
-	mkdir -p $(DEST)
-	cp -f $(NAME) $(DEST)/$(OUT)
-	chmod +x $(DEST)/$(OUT)
+	@mkdir -p $(DEST)
+	@chmod +x $(NAME)
+	@ln -s $(NAME) $(DEST)/$(OUT)
 	@echo "TekNorm installed in $(DEST)"
 
 uninstall:
 	rm -f $(DEST)/$(OUT)
 	@echo "TekNorm removed"
 
-.PHONY: all install uninstall
+.PHONY: all installe uninstall
---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+# TekNorm installation script
+# Author: Thibb1
+
+set -e
+
+# Check if a command exists
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+
+# Check if git is installed
+if ! command_exists git; then
+    echo "Git is not installed. Please install it before continuing."
+    exit 1
+fi
+
+# Check if make is installed
+if ! command_exists make; then
+    echo "Make is not installed. Please install it before continuing."
+    exit 1
+fi
+
+# Check if TekNorm is already installed
+if command_exists teknorm; then
+    echo "TekNorm is already installed."
+    exit 0
+fi
+
+# Clone TekNorm repository in the home directory
+echo "Cloning TekNorm repository..."
+cd ~
+git clone https://github.com/Thibb1/TekNorm.git
+mv TekNorm .teknorm
+
+# Install TekNorm
+cd .teknorm
+make install
+
+# Fancy message to the user in blue
+echo "\033[1;34m _____     _   _____                \033[0m"
+echo "\033[1;34m|_   _|___| |_|   | |___ ___ _____  \033[0m"
+echo "\033[1;34m  | | | -_| '_| | | | . |  _|     | \033[0m"
+echo "\033[1;34m  |_| |___|_,_|_|___|___|_| |_|_|_| \033[0m"
+echo "\033[1;34m                                    \033[0m"
+
+echo "TekNorm has been installed successfully."
